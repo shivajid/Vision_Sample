41ad6cb10ae7fea47b57df5cd1d13bcc52520dc5
Shivaji Dutta <shivaji@aurius.io>
2017-09-16 17:05:10 -0700

    Added Draw Image

---
+++ CMakeFiles/DrawImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DrawImage.dir/DrawImage.cpp.o"
+  "DrawImage.pdb"
+  "DrawImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DrawImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
